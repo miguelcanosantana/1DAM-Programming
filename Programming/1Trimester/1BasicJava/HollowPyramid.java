3ed16ff8c8d416176de7ffca6d92c7fc1a47b3e6
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2019-09-26 02:15:05 +0200

    Follow Google Guidelines

--- Programming/1Trimester/1BasicJava/HollowPyramid.java
+++ Programming/1Trimester/1BasicJava/HollowPyramid.java
@@ -1,9 +1,9 @@
 public class HollowPyramid	{
-	public static void main(String[] args) {
-		System.out.println("    *");
-		System.out.printf("   *%3s", "*\n");
-		System.out.printf("  *%5s", "*\n");
-		System.out.printf(" *%7s", "*\n");
-		System.out.println("*********");
-	}
+  public static void main(String[] args) {
+    System.out.println("    *");
+    System.out.printf("   *%3s", "*\n");
+    System.out.printf("  *%5s", "*\n");
+    System.out.printf(" *%7s", "*\n");
+    System.out.println("*********");
+  }
 }
