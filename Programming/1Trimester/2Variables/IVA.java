39cee5f0443cc48a208e91ca5c08e1150b4a6ff4
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2019-09-26 02:04:47 +0200

    It follows Google guidelines now

--- Programming/1Trimester/2Variables/IVA.java
+++ Programming/1Trimester/2Variables/IVA.java
@@ -1,23 +1,21 @@
 public class IVA {
   public static void main(String[] args){
-		
-		double aguacate = 0.6;
-		double aguab = 2;
-		double mazorca = 1.5;
-		double cd24k = 999.99;
-		double iphone= 1250.5;
-		
-		double precioSin = aguab + aguacate + mazorca + cd24k + iphone;
-		double precioIva = precioSin * 1.21;
-		
-		
-		System.out.println ("Avocado = " + aguacate + "(€)");
-		System.out.println ("Bezoya Water= " + aguab + "(€)");
-		System.out.println ("Corn = " + mazorca + "(€)");
-		System.out.println ("CD of La Rosalía Special edition 24K = " + cd24k + "(€)");
-		System.out.println ("Iphone 234 XR+- = " + iphone + "(€)");
-		System.out.println ("\n");
-		System.out.println ("Price without IVA = " + precioSin + "(€)");
-		System.out.println ("Price with IVA = " + (float) precioIva + "(€)");
-    }
+  double aguacate = 0.6;
+  double aguab = 2;
+  double mazorca = 1.5;
+  double cd24k = 999.99;
+  double iphone= 1250.5;
+  double precioSin = aguab + aguacate + mazorca + cd24k + iphone;
+  double precioIva = precioSin * 1.21;
+  
+  System.out.println ("Avocado = " + aguacate + "(€)");
+  System.out.println ("Bezoya Water= " + aguab + "(€)");
+  System.out.println ("Corn = " + mazorca + "(€)");
+  System.out.println ("CD of La Rosalía Special edition 24K = " + cd24k + "(€)");
+  System.out.println ("Iphone 234 XR+- = " + iphone + "(€)");
+  System.out.println ("\n");
+  System.out.println ("Price without IVA = " + precioSin + "(€)");
+  System.out.println ("Price with IVA = " + (float) precioIva + "(€)");
   }
+}
+
