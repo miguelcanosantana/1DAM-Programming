5c480a1d3d8ebe046e086f505eae31ded16a0860
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2019-09-26 02:12:20 +0200

    Follow Google Guidelines and translated

--- Programming/1Trimester/1BasicJava/Dragon.java
+++ Programming/1Trimester/1BasicJava/Dragon.java
@@ -21,32 +21,32 @@
                        
 */
 
-//Para poner "\" usar "\\", para poner "\\" usar "\\\\" 
+//For using "\" instead use "\\", it also applies to "\\", then use "\\\\" 
 
 public class Dragon {
 public static void main(String[] args) {
 
-	System.out.println(" <>=======()    ");
-	System.out.println("(/\\___   /|\\\\          ()==========<>_");
-	System.out.println("      \\_/ | \\\\        //|\\   ______/ \\)");
-	System.out.println("        \\_|  \\\\      // | \\_/");
-	System.out.println("          \\|\\/|\\_   //  /\\/");
-	System.out.println("           (oo)\\ \\_//  /");
-	System.out.println("          //_/\\_\\/ /  |");
-	System.out.println("         @@/  |=\\  \\  |");
-	System.out.println("         ||   \\_=\\_ \\ |");
-	System.out.println("         ||     \\==\\ \\|\\_   Tide (originally by snd)");
-	System.out.println("         /\\  __(\\===\\(  )\\ ");
-	System.out.println("            (((~) __(_/   |");
-	System.out.println("                 (((~) \\  /");
-	System.out.println("                       / /");
-	System.out.println("                      / /");
-	System.out.println("                     | |");
-	System.out.println("                   __\\ \\___");
-	System.out.println("                  /___\\ \\___\\ ");
-	System.out.println("                       \\/");
+  System.out.println(" <>=======()    ");
+  System.out.println("(/\\___   /|\\\\          ()==========<>_");
+  System.out.println("      \\_/ | \\\\        //|\\   ______/ \\)");
+  System.out.println("        \\_|  \\\\      // | \\_/");
+  System.out.println("          \\|\\/|\\_   //  /\\/");
+  System.out.println("           (oo)\\ \\_//  /");
+  System.out.println("          //_/\\_\\/ /  |");
+  System.out.println("         @@/  |=\\  \\  |");
+  System.out.println("         ||   \\_=\\_ \\ |");
+  System.out.println("         ||     \\==\\ \\|\\_   Tide (originally by snd)");
+  System.out.println("         /\\  __(\\===\\(  )\\ ");
+  System.out.println("            (((~) __(_/   |");
+  System.out.println("                 (((~) \\  /");
+  System.out.println("                       / /");
+  System.out.println("                      / /");
+  System.out.println("                     | |");
+  System.out.println("                   __\\ \\___");
+  System.out.println("                  /___\\ \\___\\ ");
+  System.out.println("                       \\/");
 	
-	}
+  }
 } 
 
 
