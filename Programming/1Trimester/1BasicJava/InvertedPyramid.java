4bb35ce4ae6b40fe0e0a32250607cf12dac38a74
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2019-09-26 02:16:58 +0200

    Follow Google Guidelines

--- Programming/1Trimester/1BasicJava/InvertedPyramid.java
+++ Programming/1Trimester/1BasicJava/InvertedPyramid.java
@@ -1,9 +1,9 @@
-public class InvertedPyramid	{
-	public static void main(String[] args) {
-		System.out.println("*********");
-		System.out.printf(" *%7s", "*\n");
-		System.out.printf("  *%5s", "*\n");
-		System.out.printf("   *%3s", "*\n");
-		System.out.println("    *");
-	}
+public class InvertedPyramid {
+  public static void main(String[] args) {
+    System.out.println("*********");
+    System.out.printf(" *%7s", "*\n");
+    System.out.printf("  *%5s", "*\n");
+    System.out.printf("   *%3s", "*\n");
+    System.out.println("    *");
+ }
 }
