b8f90376f6a51ea2b13a5bd116ca870d3302a5b8
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2019-09-26 02:14:23 +0200

    Follow Google Guidelines

--- Programming/1Trimester/1BasicJava/Pyramid.java
+++ Programming/1Trimester/1BasicJava/Pyramid.java
@@ -1,10 +1,10 @@
 public class Pyramid {
-public static void main(String[] args) {
-
-	System.out.println("    *");
-	System.out.println("   ***");
-	System.out.println("  *****");
-	System.out.println(" *******");
-	System.out.println("*********");
-	}
+ public static void main(String[] args) {
+	 
+  System.out.println("    *");
+  System.out.println("   ***");
+  System.out.println("  *****");
+  System.out.println(" *******");
+  System.out.println("*********");
+ }
 }
