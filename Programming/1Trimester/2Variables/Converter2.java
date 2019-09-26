78d54e338c82e0d18ccb190f6020457086526ebb
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2019-09-26 09:12:32 +0200

    Update variables names

--- Programming/1Trimester/2Variables/Converter2.java
+++ Programming/1Trimester/2Variables/Converter2.java
@@ -1,8 +1,8 @@
 public class Converter2 {
   public static void main(String[] args){
     double pesetas = 10; 
-    double euros = pesetas / 166.386; //1€ to pesetas
-    System.out.println("You have " + pesetas + " pesetas" + ", which means you have, " + euros + "€,");
+    double pesetasToEur = pesetas / 166.386; //1€ to pesetas
+    System.out.println("You have " + pesetas + " pesetas" + ", which means you have, " + pesetasToEur + "€,");
     }
   }
 
