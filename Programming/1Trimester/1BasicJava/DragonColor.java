fdc7c5d0eb97d5f35657d83bef2334b099968feb
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2019-09-26 02:13:27 +0200

    Follow Google Guidelines

--- Programming/1Trimester/1BasicJava/DragonColor.java
+++ Programming/1Trimester/1BasicJava/DragonColor.java
@@ -21,37 +21,35 @@
                        
 */
 
-//Para poner "\" usar "\\", para poner "\\" usar "\\\\" 
 
 public class DragonColor {
 public static void main(String[] args) {
 
-	String naranja = "\033[33m";
-	String azul = "\033[34m";
-	String cyan= "\u001b[36m";
-	String blanco = "\033[37m";
+String naranja = "\033[33m";
+String azul = "\033[34m";
+String cyan= "\u001b[36m";
+String blanco = "\033[37m";
 
-	System.out.println(azul+" <>=======()    ");
-	System.out.println("(/\\___   /|\\\\          ()==========<>_");
-	System.out.println("      \\_/ | \\\\        //|\\   ______/ \\)");
-	System.out.println("        \\_|  \\\\      // | \\_/");
-	System.out.println("          \\|\\/|\\_   //  /\\/");
-	System.out.println("           ("+naranja+"oo"+azul+")\\ \\_//  /");
-	System.out.println("          //_/\\_\\/ /  |");
-	System.out.println("         @@/  |=\\  \\  |");
-	System.out.println(blanco+"         ||"+azul+"   \\_=\\_ \\ |");
-	System.out.println(blanco+"         ||"+azul+"     \\==\\ \\|\\_   Tide (originally by snd)");
-	System.out.println(blanco+"         /\\"+azul+"  __(\\===\\(  )\\ ");
-	System.out.println("            (((~) __(_/   |");
-	System.out.println("                 (((~) \\  /");
-	System.out.println("                       / /");
-	System.out.println("                      / /");
-	System.out.println("                     | |");
-	System.out.println(cyan+"                   __"+azul+"\\ \\"+cyan+"___");
-	System.out.println("                  /___"+azul+"\\ \\"+cyan+"___\\ ");
-	System.out.println(azul+"                       \\/");
-	
-	}
+System.out.println(azul+" <>=======()    ");
+System.out.println("(/\\___   /|\\\\          ()==========<>_");
+System.out.println("      \\_/ | \\\\        //|\\   ______/ \\)");
+System.out.println("        \\_|  \\\\      // | \\_/");
+System.out.println("          \\|\\/|\\_   //  /\\/");
+System.out.println("           ("+naranja+"oo"+azul+")\\ \\_//  /");
+System.out.println("          //_/\\_\\/ /  |");
+System.out.println("         @@/  |=\\  \\  |");
+System.out.println(blanco+"         ||"+azul+"   \\_=\\_ \\ |");
+System.out.println(blanco+"         ||"+azul+"     \\==\\ \\|\\_   Tide (originally by snd)");
+System.out.println(blanco+"         /\\"+azul+"  __(\\===\\(  )\\ ");
+System.out.println("            (((~) __(_/   |");
+System.out.println("                 (((~) \\  /");
+System.out.println("                       / /");
+System.out.println("                      / /");
+System.out.println("                     | |");
+System.out.println(cyan+"                   __"+azul+"\\ \\"+cyan+"___");
+System.out.println("                  /___"+azul+"\\ \\"+cyan+"___\\ ");
+System.out.println(azul+"                       \\/");
+ }
 } 
 
 
