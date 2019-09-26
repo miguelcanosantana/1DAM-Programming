874fea53006c4d0407db9633c753e4c48b511b61
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2019-09-26 02:09:58 +0200

    Follow Google Guidelines

--- Programming/1Trimester/1BasicJava/ColorSchedule.java
+++ Programming/1Trimester/1BasicJava/ColorSchedule.java
@@ -1,12 +1,12 @@
 public class ColorSchedule {
-    public static void main(String[] args){
+  public static void main(String[] args){
     
 
-	String naranja = "\033[33m";
-	String azul = "\033[34m";
-	String cyan= "\u001b[36m";
+  String naranja = "\033[33m";
+  String azul = "\033[34m";
+  String cyan= "\u001b[36m";
     
-	System.out.println(azul+"╔═════╦══════╦═════════╦══════╦═══════╗"); 	 System.out.println("║"+naranja+"Lunes"+azul+"║"+naranja+"Martes"+azul+"║"+naranja+"Miércoles"+azul+"║"+naranja+"Jueves"+azul+"║"+naranja+"Viernes"+azul+"║");
+  System.out.println(azul+"╔═════╦══════╦═════════╦══════╦═══════╗"); 	 System.out.println("║"+naranja+"Lunes"+azul+"║"+naranja+"Martes"+azul+"║"+naranja+"Miércoles"+azul+"║"+naranja+"Jueves"+azul+"║"+naranja+"Viernes"+azul+"║");
   System.out.println(azul+"╠═════╬══════╬═════════╬══════╬═══════╣");
   System.out.println(azul+"║ "+cyan+"PRO"+azul+" ║ "+cyan+"EDD"+azul+"  ║   "+cyan+"PRO"+azul+"   ║ "+cyan+"PRO"+azul+"  ║ "+cyan+"SINF"+azul+"  ║");
   System.out.println(azul+"╠═════╬══════╬═════════╬══════╬═══════╣");
