04beb7705613aa964be13d5ba44821770ef13b25
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2019-09-26 02:07:01 +0200

    Follow Google Guidelines

--- Programming/1Trimester/2Variables/Converter1.java
+++ Programming/1Trimester/2Variables/Converter1.java
@@ -1,9 +1,8 @@
 public class Converter1 {
   public static void main(String[] args){
-		
-		double euros = 20; 
-		double pesetas = 166.386 * euros; //1€ to pesetas
-		System.out.println("You have " + euros + "€,"+" which means you have, " + pesetas + " pesetas");
+    double euros = 20; 
+    double pesetas = 166.386 * euros; //1€ to pesetas
+    System.out.println("You have " + euros + "€,"+" which means you have, " + pesetas + " pesetas");
     }
   }
 
