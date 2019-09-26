49e0577af228644096caf4b3b950dbbf37c33fd3
Miguel Cano Santana <miguelcanosantana@gmail.com>
2019-09-26 08:45:58 +0200

    2 Extra exercises added.

---
+++ Programming/1Trimester/2Variables/CharChain.java
@@ -0,0 +1,29 @@
+public class CharChain {
+  public static void main(String[] args){
+/*  char l = 'L';
+    char i = 'i';
+    char m = 'm';
+    char o = 'ó';
+    char n = 'n';
+  
+    String all = l + i + m + o + n;
+  
+    System.out.println(all);
+    
+    //CharChain.java:9: error: incompatible types: int cannot be converted to String 
+
+*/
+    //Fix
+    char l = 'L';
+    char i = 'i';
+    char m = 'm';
+    char o = 'ó';
+    char n = 'n';
+    //Add space before any letters
+    String all = " " + l + i+ m + o + n;
+  
+    System.out.println(all);
+  }
+}
+ 
+
---
+++ Programming/1Trimester/2Variables/CharString.java
@@ -0,0 +1,14 @@
+public class CharString {
+  public static void main(String[] args){
+  char m = 'M';
+  char y = 'y';
+  char h = 'H';
+  String ou = "ou";
+  String se = "se";
+  char i = 'i';
+  char s = 's';
+  String ma = "ma";
+  char l = 'l';
+  System.out.println(" " + m + y + " " + h + ou + se + " " + i + s + " " + s + ma + l + l);
+  }
+}
