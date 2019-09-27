d701840210828e1f23076c85f5ba3eb68c97708b
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2019-09-27 12:40:36 +0200

    Improved comments

--- Programming/1Trimester/2Variables/Converter2.java
+++ Programming/1Trimester/2Variables/Converter2.java
@@ -1,7 +1,7 @@
 public class Converter2 {
   public static void main(String[] args){
     double pesetas = 10; 
-    double pesetasToEur = pesetas / 166.386; //1€ to pesetas
+    double pesetasToEur = pesetas / 166.386; //Pesetas to 1€
     System.out.println("You have " + pesetas + " pesetas" + ", which means you have, " + pesetasToEur + "€,");
     }
   }
