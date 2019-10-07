3bd7fa3bfa88c1602b419313e78b27847c7d829b
Miguel Cano Santana <miguelcanosantana@gmail.com>
2019-10-07 12:24:04 +0200

    Added NumberDigits exercise.

---
+++ Programming/1Trimester/4Conditions/NumberDigits.java
@@ -0,0 +1,14 @@
+public class NumberDigits {
+  public static void main(String[] args){
+    
+    String line1;
+    System.out.print("Input a number and you will fet it's last part:");
+    line1 = System.console().readLine();
+    double number;
+    number = Double.parseDouble( line1 );
+    
+    System.out.println(number%10);
+    
+  }
+}
+
