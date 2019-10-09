efe256962b2a8e9df78162fb153b704d6869a2ea
miguelcanosantana <miguelcanosantana@gmail.com>
2019-10-09 11:05:05 +0200

    Extra Loops Exercises added.

---
+++ Programming/1Trimester/5Loops/Loop20DoWhile.java
@@ -0,0 +1,10 @@
+public class Loop20DoWhile {
+  public static void main(String[] args) { 
+    int i = 340;
+      do {
+        i = i-20;
+        System.out.println(i);
+      } while ( i > 160);
+   
+    }
+}
---
+++ Programming/1Trimester/5Loops/Loop20While.java
@@ -0,0 +1,9 @@
+public class Loop20While {
+   public static void main(String[] args) { 
+      int i = 340;
+      while ( i > 160) {
+      i = i-20;
+       System.out.println(i); 
+     }
+    }
+}
