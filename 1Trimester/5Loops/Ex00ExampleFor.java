a01f6c809cb7ca295089393ce8639ee6cd3f7782
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2019-10-10 09:30:31 +0200

    Update and rename ExampleFor.java to Ex00ExampleFor.java

--- 1Trimester/5Loops/Ex00ExampleFor.java
+++ 1Trimester/5Loops/Ex00ExampleFor.java
@@ -1,4 +1,4 @@
-public class ExampleFor {
+public class Ex00ExampleFor {
    public static void main(String[] args) { 
      for (int i = 1; i < 11; i++) { // STRUCTURE > for ( define var; define condition; run every time loop reboots )
         // i++ = i + 1  
