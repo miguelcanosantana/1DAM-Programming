ea9249a7d5830955381985373bf45fb85b07d143
miguelcanosantana <miguelcanosantana@gmail.com>
2019-10-14 09:00:51 +0200

    Added Multiply Exercise.

---
+++ 1Trimester/5Loops/Ex08Multiply.java
@@ -0,0 +1,13 @@
+public class Ex08Multiply {
+  public static void main(String[] args) { 
+    System.out.print ("Please input a number that will be multiplied up to 10:  ");
+    String line = System.console().readLine();
+    int number = Integer.parseInt(line);
+    int multiply = 0;
+    for (int count = 0; count <= 10; count = count + 1) {
+      multiply = number * count;
+      System.out.println(multiply);
+
+    }
+  }
+}
