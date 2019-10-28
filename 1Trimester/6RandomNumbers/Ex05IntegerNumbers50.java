d2bc89d6d5241332f168cbad86ff2d8b08f8c921
miguelcanosantana <miguelcanosantana@gmail.com>
2019-10-28 11:22:40 +0100

    Added Exercise about numbers

---
+++ 1Trimester/6RandomNumbers/Ex05IntegerNumbers50.java
@@ -0,0 +1,34 @@
+public class Ex05IntegerNumbers50 {
+  public static void main(String[] args) {
+    System.out.println("This program is going to print 50 Integer numbers from 100 to 199.");
+    int number = 0;
+    int total = 0;
+    int smallest = 199;
+    int biggest = 100;
+//
+    for (int i = 1; i <= 50; i++){
+
+      do{
+        number = (int)(Math.random()*200);
+      } while (number < 100);
+        total = total + number;
+        System.out.print(number + " ");
+
+        if (number < smallest){
+          smallest = number;
+        }
+        if (number > biggest){
+          biggest = number;
+        }
+        
+    }
+
+    System.out.println("");
+    System.out.println("");
+    System.out.println("The smallest is " + smallest);
+    System.out.println("");
+    System.out.println("The biggest is " + biggest);
+    System.out.println("");
+    System.out.println("The average is " + (total / 50));
+  }
+}
