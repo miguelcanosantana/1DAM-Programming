a0081ee2e2e4dd13b4581ad6a5733813e5730abe
miguelcanosantana <miguelcanosantana@gmail.com>
2019-11-12 18:33:18 +0100

    Edited Files

--- 1Trimester/7Arrays/Ex05MaxMin.java
+++ 1Trimester/7Arrays/Ex05MaxMin.java
@@ -3,14 +3,43 @@ import java.util.Scanner;
 public class Ex05MaxMin {
   public static void main(String[] args){
 
-    int[] number = new int [20];
+    int[] numbers = new int [10];
+    int firstTime = 1;
+    int min = 0;
+    int max = 0;
     Scanner s = new Scanner(System.in);
 
-  for (int i = 0; i<20; i++){
-    System.out.printf("%-8s %-8s %-8s\n", number[i], square[i], cube[i]);
+    for (int i = 0; i<10; i++){
 
-  }
- 
+      if (firstTime == 1){
+        System.out.print("Please input a number: ");
+        firstTime = 0;
+      } else {
+        System.out.print("Input another number:");
+      }
+
+      numbers[i] = Integer.parseInt(s.nextLine());
+
+      if (firstTime == 1){
+        min = numbers[0];
+        max = numbers[0];
+      } else {
+
+        if (numbers[i] < min){
+          min = numbers[i];
+        }
+        if (numbers[i] > max){
+          max = numbers[i];
+        }
+      }
+    }
+    //Terminar
+for (int i = 0; i < 10; i++){
+
+
+} 
+
+    /* System.out.printf("%-8s %-8s %-8s\n", number[i], square[i], cube[i]); */
 
 
 
