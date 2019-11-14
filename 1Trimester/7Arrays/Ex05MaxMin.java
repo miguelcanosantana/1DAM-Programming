cda1382b777f2a28c89112f47b25e3a7ff52ff06
miguelcanosantana <miguelcanosantana@gmail.com>
2019-11-14 09:10:34 +0100

    Fixed 5.

--- 1Trimester/7Arrays/Ex05MaxMin.java
+++ 1Trimester/7Arrays/Ex05MaxMin.java
@@ -4,25 +4,27 @@ public class Ex05MaxMin {
   public static void main(String[] args){
 
     int[] numbers = new int [10];
-    int firstTime = 1;
+    boolean isFirstTime = true;
     int min = 0;
     int max = 0;
     Scanner s = new Scanner(System.in);
 
     for (int i = 0; i<10; i++){
 
-      if (firstTime == 1){
+      if (isFirstTime == true){
         System.out.print("Please input a number: ");
-        firstTime = 0;
+
       } else {
         System.out.print("Input another number:");
       }
 
       numbers[i] = Integer.parseInt(s.nextLine());
 
-      if (firstTime == 1){
-        min = numbers[0];
-        max = numbers[0];
+      if (isFirstTime == true){
+        min = numbers[i];
+        max = numbers[i];
+        isFirstTime = false;
+
       } else {
 
         if (numbers[i] < min){
@@ -33,16 +35,25 @@ public class Ex05MaxMin {
         }
       }
     }
-    //Terminar
-for (int i = 0; i < 10; i++){
 
+System.out.println("");
+  for (int i = 0; i < 10; i++){
+    System.out.print(numbers[i]);
 
-} 
+    if (numbers[i] == max){
+      System.out.println(" Max");
+    }
 
-    /* System.out.printf("%-8s %-8s %-8s\n", number[i], square[i], cube[i]); */
+    if (numbers[i] == min){
+      System.out.println(" Min");
+    }
 
-
+    if ((numbers[i] != max) && (numbers[i] != min)){
+      System.out.println("");
+    }
 
   }
+
+ }
 }
 
