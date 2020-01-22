59fd72294242dbb740d5df744b53a197db0d939e
miguelcanosantana <miguelcanosantana@gmail.com>
2020-01-22 08:51:17 +0100

    Updated comments.

--- 2Trimester/9POO/PooCode/src/poocode/PooCode.java
+++ 2Trimester/9POO/PooCode/src/poocode/PooCode.java
@@ -10,8 +10,8 @@ public class PooCode {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // First exercises
-    System.out.println("1) ");
+//    This NetBeans project will contain some exercises about using objects.
+//    For opening the project, you should use a compatible IDE like NetBeans or Eclipse.
   }
   
 }
