215e2d975324bfca9e710f1053db114bfb004af8
miguelcanosantana <miguelcanosantana@gmail.com>
2020-01-27 11:17:33 +0100

    Updated code.

--- 2Trimester/9POO/PooCode/src/poocode/ex09terminalplus/Movil.java
+++ 2Trimester/9POO/PooCode/src/poocode/ex09terminalplus/Movil.java
@@ -2,27 +2,48 @@
 package poocode.ex09terminalplus;
 
 class Movil extends Terminal{
-  private String tarifa;
+  private String rate;
   
     //Methods
   
       //Constructor
-      public Movil(String number, String tarifa){
+      public Movil(String number, String rate){
         super(number); //Super is used when you want to use something from the parent.
-        this.tarifa = tarifa;
+        this.rate = rate;
       }
       
       //Getter
-      public String getTarifa() {
-        return tarifa;
+      public String getRate() {
+        return rate;
       }
       
       //Telephone Calling that returns call price for the caller
       public int cost(Movil number){
         
+        String rateName = number.getRate();
+        double costRatePerMin;
+        int minutes;
+        double finalRateCost;
         
+        //Rates' price
+        if (rateName.equals("rata")){
+          costRatePerMin = 0.06;  
+        }
         
-       
+        if (rateName.equals("mono")){
+          costRatePerMin = 0.12;  
+        }        
+
+        if (rateName.equals("bisonte")){
+          costRatePerMin = 0.30;  
+        }
+        
+        minutes = (int) ((number.getTimeCalled())/60);
+        finalRateCost = minutes * costRatePerMin;
+        
+        return costRatePerMin;
+        //FIX NON INITIALIZED  
+        
       }
       
   
