7cc397dbde69cbb19482b8eb195f6fcfe53b8175
miguelcanosantana <miguelcanosantana@gmail.com>
2020-02-03 11:17:42 +0100

    Updated and finished.

--- 2Trimester/10Collections/CollectionsCode/src/collectionscode/ex02numbermaths/Main.java
+++ 2Trimester/10Collections/CollectionsCode/src/collectionscode/ex02numbermaths/Main.java
@@ -13,6 +13,7 @@ public class Main {
      // Create between 10 and 20 numbers
      int length = (int) (Math.random()*(20-9) + 10);
      System.out.println("The amount of numbers will be " + length + ".");
+     System.out.println();
      
      
      // Records the numbers between 0 and 100
@@ -26,25 +27,50 @@ public class Main {
      
     System.out.println("These are the numbers:");
     System.out.println(numbers);
+    System.out.println();
     
     //Math
     int tempNumber;
     
       //Adding
       int addingResult = 0;
-
+      
        for (int i = 0; i < length; i++){   
          tempNumber = numbers.get(i);
          addingResult = addingResult + tempNumber;
          
-       }
+       }  
        
-       System.out.print("The total adding is: " + addingResult + ".");
+       System.out.println("The total adding is: " + addingResult + ".");
+       
+       //Average
+       double averageResult = (double) (addingResult / length);
+       System.out.println("The average is: " + averageResult + ".");
+       System.out.println();
+       
+       
+       //Max and Min
+       //(Default Max and Min)
+       int maxNum = numbers.get(0);
+       int minNum = numbers.get(0);
+       
+       for (int i = 0; i < length; i++){   
+         tempNumber = numbers.get(i);
+         
+         if (tempNumber > maxNum){
+           maxNum = tempNumber;
+         }
+         
+         if (tempNumber < minNum){
+           minNum = tempNumber;
+         }
+             
+       }  
 
-    
-    
-    
-    
+       System.out.println("The 'MAX' number is: " + maxNum + ".");
+       System.out.println("The 'min' number is: " + minNum + ".");
+       
+       
   }
   
 }
