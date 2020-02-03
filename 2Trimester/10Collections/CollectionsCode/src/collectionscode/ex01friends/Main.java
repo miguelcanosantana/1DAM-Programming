842d6556ceac4021795a7f70b53acd29d238e001
miguelcanosantana <miguelcanosantana@gmail.com>
2020-02-03 09:02:48 +0100

    Updated and reworked ex01.

--- 2Trimester/10Collections/CollectionsCode/src/collectionscode/ex01friends/Main.java
+++ 2Trimester/10Collections/CollectionsCode/src/collectionscode/ex01friends/Main.java
@@ -8,19 +8,24 @@ public class Main {
    public static void main(String[] args) {
      Scanner s = new Scanner(System.in);
      
-     //Creation of new ArrayList (CAN REMOVE 1ST "<String>"?)
+     // Creation of new ArrayList
      ArrayList<String> names = new ArrayList();
      System.out.println("Please input names: ");
      
      
-     //Records the 6 names when there is input
+     // Records the 6 names when there is input
      for (int i = 6; i > 0; i--){
        names.add(s.nextLine());
      }
      
-     //Prints the recorded names
-     System.out.println("Names: ");
-     System.out.println(names);
+     // Create an array from the ArrayList
+     String names2[] = new String[names.size()]; //Gets the size of list
+     names2 = names.toArray(names2);
+     
+     // Prints the 6 names when there is input
+     for (int i = 0; i < 6; i++){
+       System.out.println(names2[i]);
+     }
 
      
      
--- 2Trimester/10Collections/CollectionsCode/src/collectionscode/ex02numbermaths/Main.java
+++ 2Trimester/10Collections/CollectionsCode/src/collectionscode/ex02numbermaths/Main.java
@@ -1,10 +1,18 @@
 
 package collectionscode.ex02numbermaths;
+import java.util.ArrayList;
 
 public class Main {
   public static void main(String[] args) {
     
-    
+     //Creation of new ArrayList
+     ArrayList<String> names = new ArrayList();
+     System.out.println("Random values between 0 and 100: ");
+     
+     //Records the numbers
+     for (int i = 6; i > 0; i--){
+       names.add(s.nextLine());
+     }
     
     
     
