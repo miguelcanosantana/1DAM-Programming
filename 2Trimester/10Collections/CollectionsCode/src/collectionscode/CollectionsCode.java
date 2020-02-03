85387d5dc9d05b162f4c00213227a0d639933b4b
miguelcanosantana <miguelcanosantana@gmail.com>
2020-02-03 08:41:13 +0100

    More stylish.

--- 2Trimester/10Collections/CollectionsCode/src/collectionscode/CollectionsCode.java
+++ 2Trimester/10Collections/CollectionsCode/src/collectionscode/CollectionsCode.java
@@ -4,6 +4,11 @@ package collectionscode;
 public class CollectionsCode {
   public static void main(String[] args) {
     
+    //-----------------------------------------------------------------------------------|
+    //This NetBeans project will contain some exercises about using collections.         |
+    //For opening the project, you should use a compatible IDE like NetBeans or Eclipse. |
+    //-----------------------------------------------------------------------------------|
+    
     
     
     
---
+++ 2Trimester/10Collections/CollectionsCode/src/collectionscode/ex02numbermaths/Main.java
@@ -0,0 +1,20 @@
+
+package collectionscode.ex02numbermaths;
+
+public class Main {
+  public static void main(String[] args) {
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+  }
+  
+}
